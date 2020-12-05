28259b48932477f2ebd695dbb2de0618f53b53a2
DavidTheTech <david95654@googlemail.com>
2020-12-05 12:37:51 +0000

    Add files via upload

---
+++ Run In Memory (Day 5)/Run In Memory (Day 5)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace RunInMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly.Load(data.rawData).EntryPoint.Invoke(0, null);
+            Console.ReadKey();
+        }
+    }
+}
