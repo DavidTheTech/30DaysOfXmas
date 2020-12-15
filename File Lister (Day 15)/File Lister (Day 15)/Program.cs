2d0f71d93da63c85acbecc0be4456637d54b07e0
DavidTheTech <david95654@googlemail.com>
2020-12-15 22:18:34 +0000

    Add files via upload

---
+++ File Lister (Day 15)/File Lister (Day 15)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FileLister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please drag a folder or supply a folder path via command line");
+                Console.ReadKey();
+                return;
+            }
+
+            string[] Files = Directory.GetFiles(args[0], "*.*", SearchOption.AllDirectories);
+            string[] Folders = Directory.GetDirectories(args[0]);
+
+            string[] FullPath = Files[0].Split('\\');
+
+            string PathToRemove = "";
+
+            for (int i = 0; i < FullPath.Length - 1; i++)
+            {
+                PathToRemove += FullPath[i] + @"\";
+            }
+
+            for (int x = 0; x < Files.Length; x++)
+            {
+                for (int y = 0; y < Folders.Length; y++)
+                {
+                    if (Files[x].Contains(Folders[y]))
+                    {
+                        Files[x] = Files[x].Replace(Folders[y], "");
+                    }
+                }
+            }
+
+            Console.WriteLine("Files in : " + PathToRemove);
+
+            foreach(string CurFile in Files)
+            {
+                Console.WriteLine(CurFile);
+            }
+
+            Console.WriteLine("Done!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
