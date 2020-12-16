e6e56f6103478f0e88c2faed9a1cadb60ac6a4a5
DavidTheTech <david95654@googlemail.com>
2020-12-16 23:00:45 +0000

    Add files via upload

---
+++ Take Owner Ship (Day 16)/Take Owner Ship (Day 16)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TakeOwnerShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please drag a folder onto the application or into command line");
+                Console.ReadKey();
+                return;
+            }
+            Process proc = new Process();
+            proc.StartInfo.Arguments = @"/k takeown /f " + '"' + args[0] + '"' + @" /r /d y && icacls " + '"' + args[0] + '"' + @" /grant *S-1-3-4:F /t /c /l /q";
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.Start();
+            Console.ReadKey();
+        }
+    }
+}
