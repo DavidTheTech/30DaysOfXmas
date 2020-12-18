982b44014342b33dc3dde5b463973a1144c822c1
DavidTheTech <david95654@googlemail.com>
2020-12-18 22:49:36 +0000

    Add files via upload

---
+++ quickzip (Day 18)/quickzip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ionic.Zip;
+
+namespace quickzip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please do quickzip foldername");
+                return;
+            }
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(args[0]);
+                zip.Save(args[0] + ".zip");
+            }
+        }
+    }
+}
