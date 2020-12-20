e2a5894182ccd5abcd707a692442c7c4988fa32c
DavidTheTech <david95654@googlemail.com>
2020-12-20 22:25:08 +0000

    Add files via upload

---
+++ Run Embedded Resource (Day 20)/Run Embedded Resource (Day 20)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace RunFromEmbedded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Tmp = Path.GetTempFileName();
+            File.WriteAllBytes(Tmp, Properties.Resources.HelloWorld);
+            Process proc = new Process();
+            proc.StartInfo.FileName = Tmp;
+            proc.StartInfo.UseShellExecute = false;
+            proc.Start();
+        }
+    }
+}
