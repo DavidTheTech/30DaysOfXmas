7a00ae25a5e631e600f7d8ca96b5775ed95f1c90
DavidTheTech <david95654@googlemail.com>
2020-12-26 00:35:39 +0000

    Add files via upload

---
+++ CheckForUpdate (Day 24)/CheckForUpdate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace CheckForUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckForUpdate();
+        }
+
+        static void CheckForUpdate()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                Stream stream = webClient.OpenRead("http://Localhost/");
+
+                StreamReader streamReader = new StreamReader(stream);
+                string WebVersion = streamReader.ReadToEnd().Trim().ToLower();
+
+                if (!WebVersion.Equals(Application.ProductVersion.ToLower().Trim()))
+                {
+                    ShowUpdate();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        static void ShowUpdate()
+        {
+            if (MessageBox.Show("Update is available, want to download?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string fileName = "http://PlaceToDownload.com/";
+                Process.Start(fileName);
+            }
+        }
+    }
+}
